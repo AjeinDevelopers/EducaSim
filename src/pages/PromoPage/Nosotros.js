877612f3afa6207ac41011a69a729b3e32aa9722
HellCatCR <cabanas.rocha.uriel@gmail.com>
2024-05-29 13:36:51 -0600

    hola

--- src/pages/PromoPage/Nosotros.js
+++ src/pages/PromoPage/Nosotros.js
@@ -2,10 +2,19 @@ import React from "react";
 import PromoHeader from "../../modelos/header/PromoHeader.jsx";
 
 export default function Nosotros (){
+
     return(
         <>
             <PromoHeader />
-            <t5>adadas</t5>
+            <div style={{width: "1440px", height: "372px", flexDirection: "column", alignItems: "center", background:"var(--Color-Principales-Claros-Primary, #CAE6FF)"}}>
+                <div style={{flexDirection: "row", alignItems: "center"}}>
+                    <h3>Sobre</h3><h3 style={{color: "var(--color-principal)"}}  className={"secondary"}> Nosotros</h3>
+                </div>
+                <div style={{flexDirection: "column", alignItems: "center", textAlign:"center"}}>
+                    <t5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</t5>
+                </div>
+            </div>
+
         </>
     )
 }
