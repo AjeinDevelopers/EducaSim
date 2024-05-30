a456f397e15b5ef5384c2e81e051623be10585c3
HellCatCR <cabanas.rocha.uriel@gmail.com>
2024-05-30 08:55:03 -0600

    cambiosdosxd

--- src/pages/PromoPage/Nosotros.js
+++ src/pages/PromoPage/Nosotros.js
@@ -6,7 +6,7 @@ export default function Nosotros (){
     return(
         <>
             <PromoHeader />
-            <div style={{width: "1440px", height: "372px", flexDirection: "column", alignItems: "center", background:"var(--Color-Principales-Claros-Primary, #CAE6FF)"}}>
+            <div style={{width: "calc(100% - 192px)", height: "240px", padding: "var(--M, 48px) var(--XXXL, 96px)", flexDirection: "column", alignItems: "center", background:"var(--Color-Principales-Claros-Primary, #CAE6FF)"}}>
                 <div style={{flexDirection: "row", alignItems: "center"}}>
                     <h3>Sobre</h3><h3 style={{color: "var(--color-principal)"}}  className={"secondary"}> Nosotros</h3>
                 </div>
@@ -14,7 +14,11 @@ export default function Nosotros (){
                     <t5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</t5>
                 </div>
             </div>
-
+            <div style={{width: "calc(100% - 192px)", height: "240px", padding: "var(--M, 48px) var(--XXXL, 96px)", flexDirection: "column", alignItems: "center"}}>
+                <div style={{flexDirection: "column", alignItems: "left", width: "100%"}}>
+                    <h2>Mariana Carrillo</h2>
+                </div>
+            </div>
         </>
     )
 }
