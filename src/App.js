920eb10e8238530bc3abd90cfd12d432e669d4f7
MaylinAnzures <anzures.toriz.valeria.maylin@gmail.com>
2024-06-02 17:52:26 -0600

    ayuya

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import Home from './pages/PromoPage/Home.js';
 import Nosotros from './pages/PromoPage/Nosotros.js'
 import NotFound from './pages/404.js';
 import Playground from './pages/Playground.js';
-import InicioAlumn from './pages/InicioCuentas/InicioAlumn.js'
-import InicioProfe from './pages/InicioCuentas/InicioProfe.js'
-import InicioPadre from './pages/InicioCuentas/InicioPadre.js'
-import RegistroAlum from './pages/CuentaConf/RegistroAlum.js'
-import RegistroProfe from './pages/CuentaConf/RegistroProfe.js'
+import InicioAlumn from './pages/InicioCuentas/InicioAlumn.js';
+import InicioProfe from './pages/InicioCuentas/InicioProfe.js';
+import InicioPadre from './pages/InicioCuentas/InicioPadre.js';
+import RegistroAlum from './pages/CuentaConf/RegistroAlum.js';
+import RegistroProfe from './pages/CuentaConf/RegistroProfe.js';
 import ConfAlumn from './pages/CuentaConf/ConfAlumn.js';
+import menuEx from './pages/moduloPruebas/menuEx.js';
 import React from "react";
+
 
 
 function App() {
@@ -66,7 +68,10 @@ function App() {
                         path='/Configuracion_Alum'
                         element={<ConfAlumn/>}
                     />
-                    
+                    <Route
+                        path='/menu_Exam'
+                        element={<menuEx/>}
+                    />
 
                    </Routes>
           </Router>
---
+++ src/pages/moduloPruebas/menuEx.js
@@ -0,0 +1,11 @@
+import HeaderApp from "../../modelos/header/HeaderApp.jsx";
+
+export default function menuEx(){
+    return(
+        <>
+        <HeaderApp/>
+        </>
+
+    )
+
+}
