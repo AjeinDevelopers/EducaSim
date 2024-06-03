f88548e80e56fb1a5b16a9f83304a20775899900
HellCatCR <cabanas.rocha.uriel@gmail.com>
2024-06-03 02:03:57 -0600

    holatilinos

--- src/pages/PromoPage/Nosotros.js
+++ src/pages/PromoPage/Nosotros.js
@@ -75,15 +75,37 @@ export default function Nosotros() {
                     </div>
                     <div style={{ width: "100%", flexDirection: "column", gap: "var(--XS, 32px)" }}>
                         <div style={{ flexDirection: "column", alignItems: "right", width: "100%" }}>
-                            <h2>Maylin Anzures</h2>
+                            <h2>Carlos Marin</h2>
                         </div>
                         <div style={{ flexDirection: "row", alignItems: "right", width: "100%", gap: "var(--US, 12px)" }}>
+                            <Pill text={"Diseñador UI/UX"} Style={"secondary"} />
                             <Pill text={"Desarrolladora Back-End"} Style={"secondary"} />
-                            <Pill text={"Investigadora"} Style={"secondary"} />
+                            <Pill text={"Diseñador de Bases de Datos"} Style={"secondary"} />
                         </div>
                         <div style={{ flexDirection: "row", alignItems: "right", textAlign: "justify" }}>
-                            <t5>Mucho gusto. Soy alumna del CECyT No. 9 “Juan de Dios Batiz”, disfruto de aprender de cualquier tema de interés especialmente de ciencias, me gusta mucho leer, la música y  la programación. </t5>
+                            <t5>Estudiante del CECyT 9 “Juan de Dios Bátiz” que le gusta la programación, la música, los videojuegos. Tengo varios intereses como lo son el desarrollo de videojuegos, el diseño, la informática y la producción musical. </t5>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div style={{ width: "calc(100% - 192px)", height: "240px", padding: "var(--XS, 32px) var(--XXS, 24px)", flexDirection: "column", alignItems: "center", gap: "var(--M, 48px)"}}>
+                <div style={{ flexDirection: "row", alignItems: "center" }}>
+                    <h2 style={{ color: "var(--color-principal)"}} className={"secondary"}>Nuestros</h2><h2 style={{ color: "var(--color-principal)" }} className={"primary"}> Colaboradores</h2>
+                </div>
+                <div style={{ width: "calc(100% - 192px)", height: "240px", padding: "var(--M, 48px) var(--XXXL, 96px)", flexDirection: "row", gap: "var(--M, 48px)", alignItems: "center" }}>
+                    <div style={{ width: "100%", flexDirection: "column", gap: "var(--XS, 32px)" }}>
+                        <div style={{ flexDirection: "column", alignItems: "left", width: "100%" }}>
+                            <h2>Nancy Toriz Vega</h2>
+                        </div>
+                        <div style={{ flexDirection: "row", alignItems: "left", width: "100%", gap: "var(--US, 12px)" }}>
+                            <Pill text={"Investigacion"} Style={"secondary"} size={"small"}/>
+                        </div>
+                        <div style={{ flexDirection: "row", alignItems: "left", textAlign: "justify" }}>
+                            <t5>Mamá de una integrante del equipo (Anzures Toriz Valeria Maylin), proporcionó información de utilidad para el desarrollo de todo el contenido del proyecto.</t5>
+                        </div>
+                    </div>
+                    <div style={{ flexDirection: "column", alignItems: "right" }}>
+                        <img src={Avatar}></img>
                     </div>
                 </div>
             </div>
