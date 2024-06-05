2cad7b97f66e3d15d9a9e0bd987b66142f14dd92
mcmarin21 <carlitosmarin365@gmail.com>
2024-06-04 19:15:21 -0600

    Links

--- src/modelos/header/PromoHeader.jsx
+++ src/modelos/header/PromoHeader.jsx
@@ -10,14 +10,9 @@ export default function HeaderApp() {
         <nav className={"link-container"}>
           <NavLink className={(navData) => (navData.isActive ? "activeStyle" : '')} to="/" ><t6>Inicio</t6></NavLink>
           <NavLink className={(navData) => (navData.isActive ? "activeStyle" : '')} activeClassName = {"activeStyle"} to='/nosotros'><t6>Sobre Nosotros</t6></NavLink>
-          <NavLink className={""} to="/login_alumno"><Boton size = {"small"} Style={"secondary"} text={"Iniciar Sesión"} showIcon2={true} icon2={"fa-solid fa-right-to-bracket fa-fw"} handleClick={click}/>
-          </NavLink>
+          <NavLink to="/login/alumno"><Boton size = {"small"} Style={"secondary"} text={"Iniciar Sesión"} showIcon2={true} icon2={"fa-solid fa-right-to-bracket fa-fw"}/></NavLink>
         </nav>
         
     </header>
   );
-}
-
-function click(){
-    
 }
